a59617617921c780fe2bd6c23d9736788b1c0491
Corbin Rose <Rosec@ucgpdc.local>
2024-08-30 00:38:09 -0400

    udpates

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,45 +4,28 @@ function Header(props) {
     const themeChange = props.themeChange;
     const theme = props.theme;
     return(
-        <header className="bg-light">
+        <header className="header">
 
-            <nav className="navbar navbar-expand-lg navbar-shrink navbar-light fixed-top" id="mainNav">
+            <nav className="nav fixed-top" id="mainNav">
                 <div className="container-fluid">
 
-                
+
+                <div class="d-flex justify-content-space-between">
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
                         <img src="img/goat2.png" />
                         <span>Corbin<em>rose</em></span>
                     </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="true" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+                    
+        
 
-            <div className="navbar-collapse collapse show" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-
-                    <li className="nav-item">
-                        <a className="nav-link pageToggle good active" data-page="good" href="#">
-                            <svg className="svg-inline--fa fa-lightbulb fa-w-11" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="lightbulb" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" data-fa-i2svg=""><path fill="currentColor" d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path></svg>Good News 
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link pageToggle educational" data-page="learn" href="#">
-                            <svg className="svg-inline--fa fa-book fa-w-14" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="book" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M448 360V24c0-13.3-10.7-24-24-24H96C43 0 0 43 0 96v320c0 53 43 96 96 96h328c13.3 0 24-10.7 24-24v-16c0-7.5-3.5-14.3-8.9-18.7-4.2-15.4-4.2-59.3 0-74.7 5.4-4.3 8.9-11.1 8.9-18.6zM128 134c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm0 64c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm253.4 250H96c-17.7 0-32-14.3-32-32 0-17.6 14.4-32 32-32h285.4c-1.9 17.1-1.9 46.9 0 64z"></path></svg>Educational 
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link pageToggle humor" data-page="funny" href="#">
-                            <svg className="svg-inline--fa fa-smile fa-w-16" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="smile" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" data-fa-i2svg=""><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm80 168c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm-160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm194.8 170.2C334.3 380.4 292.5 400 248 400s-86.3-19.6-114.8-53.8c-13.6-16.3 11-36.7 24.6-20.5 22.4 26.9 55.2 42.2 90.2 42.2s67.8-15.4 90.2-42.2c13.4-16.2 38.1 4.2 24.6 20.5z"></path></svg> Entertainment 
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link themeToggle" 
+         
+                  
+                    <a className="btn btn-secondary themeToggle" 
                         onClick={() => themeChange(theme)}
                         href="#">
-                            <svg className="svg-inline--fa fa-moon fa-w-16" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="moon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"></path></svg></a>
-                    </li>
-                </ul>
+                        <svg className="svg-inline--fa fa-moon fa-w-16" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="moon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"></path></svg>
+                    </a>
+                    
                </div>
                </div>
             </nav>
