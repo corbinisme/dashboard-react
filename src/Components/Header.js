9c73c33a08bbfd558de768553f9ccac3023e1443
Corbin Rose <Rosec@ucgpdc.local>
2024-09-10 15:43:26 -0400

    trying to update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,42 +15,38 @@ function Header(props) {
             <nav className="nav fixed-top" id="mainNav">
                 <div className="container-fluid">
 
-
-                <div className="d-flex justify-content-space-between">
-                <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                        <img src="img/goat2.png" />
-                        <span>Corbin<em>rose</em></span>
-                    </a>
-                    
-        
-
-                   
-                    
-                    <div className="d-flex headercontainer">
-                        <div class={`desktopmenu ${menuOpen?'shown':''}`}>
+                    <div className="d-flex justify-content-space-between">
+                        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+                            <img src="img/goat2.png" />
+                            <span>Corbin<em>rose</em></span>
+                        </a>
+                        
+            
+                        <div className="d-flex headercontainer">
+                            <div className={`desktopmenu ${menuOpen?'shown':''}`}>
                                 <a className="nav-link pageToggle happy" data-page="good" href="/">
                                     Good News 
                                 </a>
-                          
+                            
                                 <a className="nav-link pageToggle educational active" data-page="learn" href="/educational">
-                                     Educational 
+                                        Educational 
                                 </a>   
-                        </div>
-                   
-                  
-                    <a className="btn btn-secondary themeToggle" 
-                        onClick={() => themeChange(theme)}
-                        href="#">
-                        {theme == "dark" ? "Light" : "Dark"}
-                    </a>
-
-                    <a className='btn btn-secondary menutoggle ms-3' onClick={toggleMenu}>
-                        <i className="fas fa-bars"></i> &equiv;
-                   </a>
-                    </div>
-                   
+                            </div>
                     
-               </div>
+                    
+                            <a className="btn btn-secondary themeToggle" 
+                                onClick={() => themeChange(theme)}
+                                href="#">
+                                {theme == "dark" ? "Light" : "Dark"}
+                            </a>
+
+                            <a className='btn btn-secondary menutoggle ms-3' onClick={toggleMenu}>
+                                <i className="fas fa-bars"></i> &equiv;
+                            </a>
+                        </div>
+                    
+                        
+                    </div>
                </div>
             </nav>
         </header>
